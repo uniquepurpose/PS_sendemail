938a04fe7cd539d28c4d30ccb4acc65979f24f8c
Christoph Holas <uniquepurpose@users.noreply.github.com>
2018-05-09 13:43:30 +0200

    Update sendemail.ps1
    Cosmetics, changed time format to 24H, encrypted password file is now a variable.

--- sendemail.ps1
+++ sendemail.ps1
@@ -1,15 +1,17 @@
 # Simple script to send mails with SMTP auth, STARTTLS and encrypted credentials
-
 # To store encrypted password for SMTP auth in file run this command once:
-# Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File encrypted_password.txt
+# Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File $mailpassfile
 
+# Please adjust these variables accordingly.
 $mailuser = "alert@somedomain.com"
-$mailpass = Get-Content encrypted_password.txt | ConvertTo-SecureString
-$mailcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $mailuser,$mailpass
-
+$mailpassfile = "C:\Scripts\encrypted_password.txt"
 $mailrecipient = "user@somedomain.com"
 $mailsubject = "This is an alert!"
-$mailbody = "Alert has been triggered on $(Get-Date -Format yyyy-MM-dd) $(Get-Date -Format hh:mm:ss)"
+$mailbody = "Alert has been triggered on $(Get-Date -Format yyyy-MM-dd) $(Get-Date -Format HH:mm:ss)"
 $mailserver = "smtp.somedomain.com"
+
+# This is where the magic happens:
+$mailpass = Get-Content $mailpassfile | ConvertTo-SecureString
+$mailcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $mailuser,$mailpass
 
 Send-MailMessage -From $mailuser -To $recipient -Subject $mailsubject -Body $mailbody -SmtpServer $mailserver -Port 587 -UseSsl -Credential $mailcred
